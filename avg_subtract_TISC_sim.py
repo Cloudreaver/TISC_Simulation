Bug fixes in average subtraction code

avg_subtract_TISC_sim.py
@@ -3,7 +3,7 @@
 # A very simple impulsive signal + noise generator to get the 
 
 import random
-from array import array
+#from array import array
 import numpy as np
 from impulse import impulse_gen, butter_bandpass_filter, butter_bandpass
 from noise import generate_noise
@@ -19,7 +19,7 @@ def avg_subtract_TISC_sim(SNR,threshold,
              peak_amplitude=25.0,carrier_frequency=260000000.0,modulation_frequency=16000000.0,
              seed=5522684,draw_flag=0,digitization_factor=1,
              delay_type_flag=1,
-             output_dir="output/",average_signal=20.0):
+             output_dir="output/",average_signal=150):
    
                            
 
@@ -38,8 +38,8 @@ def avg_subtract_TISC_sim(SNR,threshold,
    a_input_noise = np.zeros(num_upsamples)
    b_input_noise = np.zeros(num_upsamples)
    c_input_noise = np.zeros(num_upsamples)
-   time = np.zeros(num_samples)
-   upsampled_time = np.zeros(num_upsamples)
+   #time = np.zeros(num_samples)
+   #upsampled_time = np.zeros(num_upsamples)
    a_input_signal = np.zeros(num_upsamples)
    b_input_signal = np.zeros(num_upsamples)
    c_input_signal = np.zeros(num_upsamples)
@@ -50,16 +50,14 @@ def avg_subtract_TISC_sim(SNR,threshold,
    b_dig_waveform = np.zeros(num_samples)
    c_dig_waveform = np.zeros(num_samples)
    empty_list = np.zeros(num_upsamples)
-   
    new_average_signal =0
  
-   
-   
-   for i in range(0,num_upsamples):
-      upsampled_time[i] = (i/(sample_freq*upsample))*(10**9)
+   #if (updample>1):
+      #for i in range(0,num_upsamples):
+         #upsampled_time[i] = (i/(sample_freq*upsample))*(10**9)
 
-   for i in range(0,num_samples):
-      time[i] = (i/(sample_freq))*(10**9)
+   #for i in range(0,num_samples):
+      #time[i] = (i/(sample_freq))*(10**9)
 
 ###################################
    # Generate Thermal Noise
@@ -71,12 +69,8 @@ def avg_subtract_TISC_sim(SNR,threshold,
 
 #####################################
    # Determine RMS of noise and signal amplitude
-
-   noise_rms = np.sqrt(np.mean((a_input_noise-noise_mean)**2,))
-   
-   
+   noise_rms = np.sqrt(np.mean((a_input_noise-noise_mean)**2,))   
    signal_amp = SNR*2*noise_rms
-
 #####################################
    
    
@@ -91,14 +85,8 @@ def avg_subtract_TISC_sim(SNR,threshold,
 
       c_input_cw_noise = generate_cw(num_samples,upsample,sample_freq,carrier_frequency,modulation_frequency,peak_amplitude,filter_flag)
       c_input_noise += c_input_cw_noise
-
 #####################################
-
-   #new_average_signal = np.sqrt(np.mean((a_input_noise-noise_mean)**2,))
-      #print a_input_noise-noise_mean
-      #print (a_input_noise-noise_mean)**2
-      #print np.mean((a_input_noise-noise_mean)**2,)
-      #print np.sqrt(np.mean((a_input_noise-noise_mean)**2,))
+ 
  
 #####################################
    # Generate impulse
@@ -126,6 +114,7 @@ def avg_subtract_TISC_sim(SNR,threshold,
       c_input_signal_noise = c_input_noise
 ##########################################
 
+
 ##########################################
    # Digitized the incoming signal and noise (RITC)
    a_dig_waveform = digitize(a_input_signal_noise,num_samples,upsample,num_bits,noise_mean,noise_rms,digitization_factor)
@@ -134,12 +123,12 @@ def avg_subtract_TISC_sim(SNR,threshold,
 
 ##########################################
 
+
 ##########################################
    # Run the signal through the GLITC module to get trigger
-   trigger_flag, max_sum, new_average_signal = avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig_waveform,threshold,TISC_sample_length,delay_type_flag=delay_type_flag,average_signal=average_signal)
-   #print average_signal
-   #print new_average_signal
+   trigger_flag, max_sum = avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig_waveform,threshold,TISC_sample_length,delay_type_flag=delay_type_flag,average_signal=average_signal)
 #########################################
+
 
 #########################################
    # Output data
@@ -152,9 +141,8 @@ def avg_subtract_TISC_sim(SNR,threshold,
    if draw_flag:
       
       dummy = raw_input('Press any key to close')
-      p
 
-   return trigger_flag, max_sum, new_average_signal
+   return trigger_flag, max_sum
 
 if __name__ == '__main__':
    #import ROOT

avg_subtracted_sum_correlator.py
@@ -2,19 +2,21 @@
 
 # Simulates the output of the GLITC 
 
-from array import array
+#from array import array
 import numpy as np
 
 
 
-def avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig_waveform,threshold,TISC_sample_length=16,delay_type_flag=0,average_signal=0.0):
+def avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig_waveform,threshold,TISC_sample_length=16,delay_type_flag=0,average_signal=0):
    
-   speed_of_light = 2.99*10**8
-   sample_period = 3.5810**(-10)
-   ab_distance = 1.0
-   ac_horz_distance = 1.17
-   ac_vert_distance = 4.7
-   ac_distance = np.sqrt((ac_horz_distance)**2+(ac_vert_distance)**2)
+   #speed_of_light = 2.99*10**8
+   #sample_period = 3.5810**(-10)
+   #ab_distance = 1.0
+   #ac_horz_distance = 1.17
+   #ac_vert_distance = 4.7
+   #ac_distance = np.sqrt((ac_horz_distance)**2+(ac_vert_distance)**2)
+   
+   max_sum_shift = 150
 
    trigger_flag = 0
    if (delay_type_flag == 1):
@@ -82,8 +84,8 @@ def avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig
    this_sum = 0
    total_sum = 0
    max_total_sum = 0
-   add_AB = [0] * TISC_sample_length
-   add_ABC = [0] * TISC_sample_length
+   add_AB = np.zeros(TISC_sample_length)
+   add_ABC = np.zeros(TISC_sample_length)
 
    best_chunk = 0
    b_lower_index_limit = 0
@@ -121,14 +123,20 @@ def avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig
             add_AB = np.add(a_dig_waveform[a_start_pos:a_start_pos+TISC_sample_length],b_dig_waveform[b_start_pos:b_start_pos+TISC_sample_length])
             add_ABC = np.add(add_AB,c_dig_waveform[c_start_pos:c_start_pos+TISC_sample_length])
             square_ABC = add_ABC**2
-            new_average_signal = int(np.sqrt(np.mean(square_ABC)))
-            square_ABC -= average_signal
             square_sum_ABC[i] = np.sum(square_ABC)
 
       # Add all the two sums for each delay
       total_sum = np.add(square_sum_ABC,previous_square_sum)
       # Find the maximum sum
       max_total_sum = np.amax(total_sum)
+      unsubtracted_max_total_sum = max_total_sum
+      max_total_sum += max_sum_shift
+      #print "Unsubtracted: "+str(max_total_sum)
+      max_total_sum -= average_signal
+      #print "Subtracted: "+str(max_total_sum)
+      #if (max_total_sum < 0):
+         #max_total_sum = 0
+      #print "Subtracted: "+str(max_total_sum)
       # Find the delays for the largest sum
       best_delays = GLITC_delays[np.argmax(total_sum)]
       
@@ -138,37 +146,37 @@ def avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig
       #print "Max Sum: "+str(max_total_sum)
       #print best_delays
 
-      ab_theta = np.arcsin((speed_of_light*best_delays[1]*sample_period)/ab_distance)
-      ac_theta = np.arctan(ac_vert_distance/ac_horz_distance)-np.pi/2+np.arcsin((speed_of_light*best_delays[2]*sample_period)/ac_distance)
-      ab_theta *= 180/np.pi
-      ac_theta*= 180/np.pi
+      #ab_theta = np.arcsin((speed_of_light*best_delays[1]*sample_period)/ab_distance)
+      #ac_theta = np.arctan(ac_vert_distance/ac_horz_distance)-np.pi/2+np.arcsin((speed_of_light*best_delays[2]*sample_period)/ac_distance)
+      #ab_theta *= 180/np.pi
+      #ac_theta*= 180/np.pi
 
    else:
 
       #Set the lower index limit of Ch B/C
-      for chunk in range(0,(num_samples/TISC_sample_length)):
-         b_lower_index_limit = 0
-         c_lower_index_limit = 0
-         previous_sum_ABC = square_sum_ABC
+      #for chunk in range(0,(num_samples/TISC_sample_length)):
+         #b_lower_index_limit = 0
+         #c_lower_index_limit = 0
+         #previous_sum_ABC = square_sum_ABC
 
          # Shift Ch B/C around
-         for b in range(b_lower_index_limit,len(a_dig_waveform)-TISC_sample_length):
-            for c in range(c_lower_index_limit,len(a_dig_waveform)-TISC_sample_length):
-               this_sum = 0
-               add_AB = np.add(a_dig_waveform[chunk*TISC_sample_length:(chunk*TISC_sample_length)+TISC_sample_length],b_dig_waveform[b:TISC_sample_length+b])
-               add_ABC = np.add(add_AB,c_dig_waveform[c:TISC_sample_length+c])
-               square_ABC = add_ABC**2
-               square_sum_ABC[i] = np.sum(square_ABC)
-               delays[i] = [chunk*TISC_sample_length,b,c]
-               i += 1
+         #for b in range(b_lower_index_limit,len(a_dig_waveform)-TISC_sample_length):
+            #for c in range(c_lower_index_limit,len(a_dig_waveform)-TISC_sample_length):
+               #this_sum = 0
+               #add_AB = np.add(a_dig_waveform[chunk*TISC_sample_length:(chunk*TISC_sample_length)+TISC_sample_length],b_dig_waveform[b:TISC_sample_length+b])
+               #add_ABC = np.add(add_AB,c_dig_waveform[c:TISC_sample_length+c])
+               #square_ABC = add_ABC**2
+               #square_sum_ABC[i] = np.sum(square_ABC)
+               #delays[i] = [chunk*TISC_sample_length,b,c]
+               #i += 1
                #print "a: "+str(chunk*TISC_sample_length)+":"+str((chunk*TISC_sample_length)+TISC_sample_length)+"\tb: "+str(b)+":"+str(b+TISC_sample_length)+"\tc: "+str(c)+":"+str(c+TISC_sample_length)
                   
-      total_sum = np.add(square_sum_ABC,previous_square_sum)
-      max_total_sum = np.amax(total_sum)
-      best_delays = delays[np.argmax(total_sum)]
+      #total_sum = np.add(square_sum_ABC,previous_square_sum)
+      #max_total_sum = np.amax(total_sum) - average_signal
+      #best_delays = delays[np.argmax(total_sum)]
       
       #print total_sum
-      print "Max sum: " +str(max_total_sum)
+      print "DONT USE THESE DELAYS!!!"
       #print best_delays
 
    if(max_total_sum > threshold):
@@ -181,7 +189,7 @@ def avg_subtracted_sum_correlate(num_samples,a_dig_waveform,b_dig_waveform,c_dig
    #print "B Delay: " + str(best_b_delay)#+'\t'+str(b_input_delay-best_b_delay)
    #print "C Delay: " + str(best_c_delay)#+'\t'+str(c_input_delay-best_c_delay)
    #print "\n\n"
-   return trigger_flag, max_total_sum, new_average_signal
+   return trigger_flag, unsubtracted_max_total_sum
 
 
 if __name__ == '__main__':

cw.py
@@ -5,7 +5,7 @@
 # Then filter and resample if desired
 
 import numpy as np
-from math import sin
+#from math import sin
 from impulse import butter_bandpass_filter
 from scipy.signal import resample
 from random import random

impulse.py
@@ -78,6 +78,7 @@ if __name__ == '__main__':
     import random
     from noise import generate_noise
     from digitizer import digitize
+    from cw import generate_cw
     
     sample_frequency = 2800000000
     sample_length = 74
@@ -86,20 +87,27 @@ if __name__ == '__main__':
     draw_flag = 0
     output_dir = 'output/'
     clock_jitter = 0.000000001
-    poly_index = 5
-    SNR = 5
-    noise_sigma = 100
+    #poly_index = 5
+    SNR = 3
+    noise_sigma = 20
     save_plot_flag = True
+    cw_flag = False
+    carrier_frequency=260000000.0    # Hz
+    modulation_frequency=16000000.0  # Hz
+    peak_amplitude = 0.5*noise_sigma #Peak amplitude in mV
     
     upsample_length = upsample*sample_length
     
     impulse_sample = np.zeros(sample_length)
     impulse_upsample = np.zeros(upsample_length)
     impulse_downsample = np.zeros(sample_length)
-    impulse_upsample_fit_poly = np.zeros(poly_index)
+    #impulse_upsample_fit_poly = np.zeros(poly_index)
     impulse_upsample_fit = np.zeros(upsample_length)
     impulse_noise_sample = np.zeros(sample_length)
     digitized_sample = np.zeros(sample_length)
+    noise_sample = np.zeros(sample_length)
+    cw_sample = np.zeros(sample_length)
+    #noise_cw_sample = np.zeros(sample_length)
     
     time = np.linspace(0.0,((sample_length*(10**9))/sample_frequency), sample_length)
     impulse_sample = impulse_gen(sample_length,impulse_position,1,sample_frequency,draw_flag=draw_flag,output_dir=output_dir)
@@ -107,39 +115,71 @@ if __name__ == '__main__':
     plt.clf()
     plt.plot(time,impulse_sample[0:sample_length])
     plt.xlabel("Time [ns]")
-    plt.ylabel("Voltage [mV]")
+    plt.ylabel("Amplitude [unitless]")
     plt.title("Impulse Simulation")
     if (save_plot_flag):
         plt.savefig("impulse_sample.png")
     
-    upsample_time = np.linspace(0.0,((upsample_length*(10**9))/sample_frequency), upsample_length)
-    impulse_upsample = impulse_gen(sample_length,impulse_position,upsample,sample_frequency,draw_flag=draw_flag,output_dir=output_dir)
-    plt.figure(2)
-    plt.clf()
-    plt.plot(upsample_time,impulse_upsample[0:upsample_length])
-    plt.xlabel("Time [ns]")
-    plt.ylabel("Voltage [mV]")
-    plt.title("Upsampled Impulse Simulation")
-    if (save_plot_flag):
-        plt.savefig("upsample_impulse_sample.png")
+    #upsample_time = np.linspace(0.0,((upsample_length*(10**9))/sample_frequency), upsample_length)
+    #impulse_upsample = impulse_gen(sample_length,impulse_position,upsample,sample_frequency,draw_flag=draw_flag,output_dir=output_dir)
+    #plt.figure(2)
+    #plt.clf()
+    #plt.plot(upsample_time,impulse_upsample[0:upsample_length])
+    #plt.xlabel("Time [ns]")
+    #plt.ylabel("Voltage [mV]")
+    #plt.title("Upsampled Impulse Simulation")
+    #if (save_plot_flag):
+        #plt.savefig("upsample_impulse_sample.png")
     
     signal_amp = (2*SNR*noise_sigma)
     difference=np.amax(impulse_sample)-np.amin(impulse_sample) # Get peak to peak voltage
     impulse_sample *= (1/difference) # Normalize input
     impulse_sample *= signal_amp # Amplify
-    impulse_noise_sample = impulse_sample + generate_noise(sample_length=sample_length,upsample=1,noise_mean=380,noise_sigma=100,filter_flag=1)
-    plt.figure(4)
+    
+    noise_sample = generate_noise(sample_length=sample_length,upsample=1,noise_mean=0.0,noise_sigma=noise_sigma,filter_flag=1)
+    plt.figure(3)
+    plt.clf()
+    plt.plot(time,noise_sample)
+    plt.xlabel("Time [ns]")
+    plt.ylabel("Voltage [mV]")
+    plt.title("Band Limited Noise Sample")
+    if (save_plot_flag):
+        plt.savefig("noise_sample.png")
+    
+    if (cw_flag):
+        cw_sample = generate_cw(sample_length=sample_length,upsample=1,sample_frequency=sample_frequency,carrier_frequency=carrier_frequency,modulation_frequency=modulation_frequency,peak_amplitude=peak_amplitude,filter_flag=1)
+        plt.figure(4)
+        plt.clf()
+        plt.plot(time,cw_sample)
+        plt.xlabel("Time [ns]")
+        plt.ylabel("Voltage [mV]")
+        plt.title("Carrier Wave Sample (Peak Amp: "+str(peak_amplitude/noise_sigma)+" x Noise RMS)")
+        if (save_plot_flag):
+            plt.savefig("cw_sample.png")
+        
+        noise_sample = np.add(noise_sample,cw_sample)
+        plt.figure(5)
+        plt.clf()
+        plt.plot(time,noise_sample)
+        plt.xlabel("Time [ns]")
+        plt.ylabel("Voltage [mV]")
+        plt.title("Noise+CW Sample")
+        if (save_plot_flag):
+            plt.savefig("noise_cw_sample.png")
+        
+    impulse_noise_sample = np.add(impulse_sample,noise_sample)
+    plt.figure(6)
     plt.clf()
     plt.plot(time,impulse_noise_sample)
     plt.xlabel("Time [ns]")
     plt.ylabel("Voltage [mV]")
-    plt.title("Impulse + Noise Sample")
+    plt.title("Impulse(SNR "+str(SNR)+") + Noise Sample(RMS "+str(noise_sigma)+"mV)")
     if (save_plot_flag):
         plt.savefig("impulse_noise_sample.png")
     
     
-    digitized_sample = digitize(impulse_noise_sample,sample_length,upsample=1,num_bits=3,noise_mean=380,noise_rms=100,digitization_factor=1)
-    plt.figure(5)
+    digitized_sample = digitize(impulse_noise_sample,sample_length,upsample=1,num_bits=3,noise_mean=0.0,noise_rms=20,digitization_factor=1)
+    plt.figure(8)
     plt.clf()
     plt.plot(time,digitized_sample)
     plt.xlabel("Time [ns]")

run_avg_subtracted_TISC_sim.py
@@ -6,37 +6,39 @@ if __name__ == '__main__':
 	from avg_subtract_TISC_sim import avg_subtract_TISC_sim
 	from TISC_sim import TISC_sim
 	import numpy as np
-	import subprocess as Popen
-	from array import array
+	#import subprocess as Popen
+	#from array import array
 	import matplotlib.pyplot as plt
 	
 	
 	#############################################################
 	# Parameters
-	num_events = 100                	# Number of events to generate per threshold
-	simulation_rate = 100.0   		# Simulation speed
-	event_rate= 100.0			 	# Rate to generate impulseive events
-	num_samples = 74                 # Length of Signal Window
+	num_events = 2		           		# Number of events to generate per threshold
+	simulation_rate = 100.0   			# Simulation speed
+	event_rate= 100.0			 		# Rate to generate impulseive events
+	num_runs = int(num_events*(simulation_rate/event_rate))
+	num_samples = 74                 	# Length of Signal Window
 	noise_sigma = 20.0
 	noise_mean = 0.0
-	draw_flag = False                # 1=draw event graphs, 0=don't                        
-	SNR_draw_flag = True             # 1=draw SNR graphs, 0=don't
-	delay_type_flag = 1              # 1=use GLITC delays, 0=all possible delays
-	cw_flag = True
-	carrier_frequency=260000000.0    # Hz
-	modulation_frequency=16000000.0  # Hz
-	peak_amplitude = 0.5*noise_sigma #Peak amplitude in mV
+	draw_flag = False                	# 1=draw event graphs, 0=don't                        
+	SNR_draw_flag = True             	# 1=draw SNR graphs, 0=don't
+	delay_type_flag = 1              	# 1=use GLITC delays, 0=all possible delays
+	cw_flag = False
+	carrier_frequency=260000000.0    	# Hz
+	modulation_frequency=16000000.0  	# Hz
+	peak_amplitude = 2.0*noise_sigma 	# Peak amplitude in mV
 	save_output = True
+	max_sum_memory = 4					# Number of max sums to average over
 	
 
-	impulse_pos = 5                  # Position of impulse in Ch A (must be contained within first 32 samples)
-	b_input_delay = 0                # Ch B signal offset
-	c_input_delay = 0                # Ch C signal offset
+	impulse_pos = 5                  	# Position of impulse in Ch A (must be contained within first 32 samples)
+	b_input_delay = 0                	# Ch B signal offset
+	c_input_delay = 0                	# Ch C signal offset
 	SNR = 3.0							# SNR of impulsive signal
 
-	low_threshold = 0.0              # Lowest Threshold
-	high_threshold = 400.0           # Highest Threshold
-	step_threshold = 25.0            # Threshold Interval
+	low_threshold = 0.0              	# Lowest Threshold
+	high_threshold = 100.0           	# Highest Threshold
+	step_threshold = 25.0            	# Threshold Interval
 	#############################################################
 
 
@@ -50,20 +52,24 @@ if __name__ == '__main__':
 	average_subtracted_noise_trigger_number = np.zeros(len(threshold))
 	noise_trigger_rate = np.zeros(len(threshold))
 	noise_trigger_number = np.zeros(len(threshold))
-	average_signal=0
-	average_subtracted_average_signal=0
-	average_subtracted_average_noise =0
-	
-	num_runs = int(num_events*(simulation_rate/event_rate))
-	#print num_runs
+	#average_signal=0
+	average_subtracted_signal_max_sum=0
+	#average_subtracted_noise_max_sum =0
+	average_subtracted_signal_event_flag = 0
+	average_subtracted_signal_noise_flag = 0
+	mean_noise_max_sum = 0
+	average_noise_max_sum = np.array([140,140,140,140])#np.zeros(max_sum_memory)
    
 	# Write settings file
 	if(save_output):
-		output_dir = str(os.path.dirname(os.path.realpath(__file__))+"/output/average_subtracted_"+time.strftime('%Y_%m_%d_%H.%M.%S'))
+		if (cw_flag):
+			output_dir = str(os.path.dirname(os.path.realpath(__file__))+"/output/as_SNR"+str(SNR)+"_ER"+str(event_rate)+"_SR"+str(simulation_rate)+"_num"+str(num_events)+"_"+time.strftime('%Y_%m_%d_%H.%M.%S'))
+		else:
+			output_dir = str(os.path.dirname(os.path.realpath(__file__))+"/output/as_SNR"+str(SNR)+"_cw"+str(peak_amplitude/noise_sigma)+"_ER"+str(event_rate)+"_SR"+str(simulation_rate)+"_num"+str(num_events)+"_"+time.strftime('%Y_%m_%d_%H.%M.%S'))
+		print output_dir
 		if not os.path.exists(output_dir):
 			os.makedirs(output_dir)
 		settings_filename = open(output_dir+"/settings.txt","w")
-		#print output_dir
 		settings_filename.write("TISC simulation using new signal averaging system")
 		settings_filename.write("Input parameters for TISC simulation\n\n")
 		settings_filename.write("Start time: "+str(time.strftime('%Y_%m_%d_%H.%M.%S')))
@@ -98,57 +104,66 @@ if __name__ == '__main__':
 		# Step over time.
 		for timestep in range(0,num_runs):
 			
+			# Shift the average sum array to make room for new value
+			average_noise_max_sum = np.roll(average_noise_max_sum,1)
+				
 			# Generate signal are event rate
 			if(timestep % int((simulation_rate/event_rate)) ==0):
-				#print "Sending impulsive signal, timestep "+str(timestep)
+				#print "\nSending impulsive signal, timestep "+str(timestep)
 				# Average subtracted impulsive event
-				average_subtracted_event_passed_flag, average_subtracted_max_sum, average_subtracted_average_signal = avg_subtract_TISC_sim(SNR,threshold[threshold_counter],
+				average_subtracted_event_passed_flag, average_subtracted_signal_max_sum = avg_subtract_TISC_sim(SNR,threshold[threshold_counter],
 						impulse_pos,b_input_delay,c_input_delay,num_samples=num_samples,
 						cw_flag=cw_flag,carrier_frequency=carrier_frequency,peak_amplitude=peak_amplitude,modulation_frequency=modulation_frequency,
 						noise_sigma=noise_sigma,noise_mean=noise_mean,
-						draw_flag=draw_flag,average_signal=average_subtracted_average_signal)
+						draw_flag=draw_flag,average_signal=mean_noise_max_sum)
 				
 				# Non subtracted impulseive event
-				event_passed_flag, max_sum = TISC_sim(SNR,threshold[threshold_counter],
+				event_passed_flag, signal_max_sum = TISC_sim(SNR,threshold[threshold_counter],
 						impulse_pos,b_input_delay,c_input_delay,num_samples=num_samples,
 						cw_flag=cw_flag,carrier_frequency=carrier_frequency,peak_amplitude=peak_amplitude,modulation_frequency=modulation_frequency,
 						noise_sigma=noise_sigma,noise_mean=noise_mean)
 				
 				# Average subtracted noise event
-				average_subtracted_noise_passed_flag,average_subtracted_noise_max_sum, average_subtracted_average_noise = avg_subtract_TISC_sim(0,threshold[threshold_counter],
+				average_subtracted_noise_passed_flag,average_noise_max_sum[0] = avg_subtract_TISC_sim(0.0,threshold[threshold_counter],
 						impulse_pos,b_input_delay,c_input_delay,num_samples=num_samples,
 						cw_flag=cw_flag,carrier_frequency=carrier_frequency,peak_amplitude=peak_amplitude,modulation_frequency=modulation_frequency,
 						noise_sigma=noise_sigma,noise_mean=noise_mean,
-						draw_flag=draw_flag,average_signal=average_subtracted_average_noise)
+						draw_flag=draw_flag,average_signal=mean_noise_max_sum)
+						
 				# non subtracted noise event					
 				noise_passed_flag, noise_max_sum = TISC_sim(0.0,threshold[threshold_counter],
 						impulse_pos,b_input_delay,c_input_delay,num_samples=num_samples,
 						cw_flag=cw_flag,carrier_frequency=carrier_frequency,peak_amplitude=peak_amplitude,modulation_frequency=modulation_frequency,
 						noise_sigma=noise_sigma,noise_mean=noise_mean)
-						
+							
 			# Generate noise event when impulsive event is not called for
 			else:
-				#print "Sending background signal, timestep "+str(timestep)
+				#print "\nSending background signal, timestep "+str(timestep)
 				# Average subtracted noise event
-				average_subtracted_event_passed_flag,average_subtracted_max_sum, average_subtracted_average_signal = avg_subtract_TISC_sim(0,threshold[threshold_counter],
+				average_subtracted_noise_passed_flag,average_noise_max_sum[0] = avg_subtract_TISC_sim(0,threshold[threshold_counter],
 						impulse_pos,b_input_delay,c_input_delay,num_samples=num_samples,
 						cw_flag=cw_flag,carrier_frequency=carrier_frequency,peak_amplitude=peak_amplitude,modulation_frequency=modulation_frequency,
 						noise_sigma=noise_sigma,noise_mean=noise_mean,
-						draw_flag=draw_flag,average_signal=average_subtracted_average_signal)
+						draw_flag=draw_flag,average_signal=mean_noise_max_sum)
+						
 				# Copy over values for subtracting on next iteration
-				average_subtracted_event_passed_flag = average_subtracted_event_noise_flag
-				average_subtracted_event_max_sum = average_subtracted_noise_max_sum
-				average_subtracted_average_signal = average_subtracted_average_noise
+				average_subtracted_event_passed_flag = average_subtracted_noise_passed_flag
 						
 				# Non subtracted noise event					
-				event_passed_flag, max_sum = TISC_sim(0.0,threshold[threshold_counter],
+				noise_passed_flag, noise_max_sum = TISC_sim(0.0,threshold[threshold_counter],
 						impulse_pos,b_input_delay,c_input_delay,num_samples=num_samples,
 						cw_flag=cw_flag,carrier_frequency=carrier_frequency,peak_amplitude=peak_amplitude,modulation_frequency=modulation_frequency,
 						noise_sigma=noise_sigma,noise_mean=noise_mean)
+						
 				# Copy over values for 
 				event_passed_flag = noise_passed_flag
-				event_max_sum = noise_max_sum
-											
+				event_max_sum = noise_max_sum		
+			# Exit (event or noise) if statement
+			
+			# Take the mean of the max sum array
+			mean_noise_max_sum = np.mean(average_noise_max_sum)
+				
+			# If trigger was seen, add it the trigger number					
 			if(average_subtracted_event_passed_flag):
 				average_subtracted_trigger_number[threshold_counter] +=1
 			if(event_passed_flag):
@@ -157,34 +172,48 @@ if __name__ == '__main__':
 				average_subtracted_noise_trigger_number[threshold_counter] +=1
 			if(noise_passed_flag):
 				noise_trigger_number[threshold_counter] +=1
+				
+			#print average_subtracted_trigger_number[threshold_counter]
+			#print trigger_number[threshold_counter]
+			#print average_subtracted_noise_trigger_number[threshold_counter]
+			#print noise_trigger_number[threshold_counter]
+			#print "\n"
+		# Exit timestep loop
+		
 		if(save_output):
 			data_filename.write(str(threshold[threshold_counter])+','+str(average_subtracted_trigger_number[threshold_counter])+','+str(trigger_number[threshold_counter])+','+str(average_subtracted_noise_trigger_number[threshold_counter])+','+str(noise_trigger_number[threshold_counter]))
+	# Exit threshold counter loop	
 		
-		
-		
+	# Calculate trigger rates from number of triggers and the simulation rate
 	average_subtracted_trigger_rate = (average_subtracted_trigger_number/num_runs)*simulation_rate
 	trigger_rate = (trigger_number/num_runs)*simulation_rate
 	average_subtracted_noise_trigger_rate = (average_subtracted_noise_trigger_number/num_runs)*simulation_rate
 	noise_trigger_rate = (noise_trigger_number/num_runs)*simulation_rate
-	#print "Average Subtract trigger rates "+str(average_subtract_trigger_rate)	
-	#print "Average Subtract thresholds "+str(average_subtract_threshold)	
+	
+
+	
+	# Make all the plots	
 	plt.figure(1)
 	plt.clf()
 	plt.axis([np.amin(threshold),np.amax(threshold),0,(simulation_rate*1.5)])
 	ax= plt.gca()
 	ax.set_autoscale_on(False)
 	plt.plot(threshold,average_subtracted_trigger_rate,'r',threshold,trigger_rate,'b',threshold,average_subtracted_noise_trigger_rate,'r--',threshold,noise_trigger_rate,'b--')
-	#plt.plot(threshold,average_subtract_trigger_rate,'r',label=("Avg. Subtract SNR = "+str(SNR)),threshold,trigger_rate,label=("Non-subtracted SNR = "+str(SNR)),'b')
-	plt.title("Average Subtracted SNR Curves @ "+str(event_rate)+" Hz Event Rate")
+	plt.title("SNR Curves for "+str(event_rate)+" Hz Event Rate @ "+str(simulation_rate)+" Hz Simulation Rate")
 	plt.xlabel("Threshold [DAC Counts]")
 	plt.ylabel("Trigger Rate [Hz]")
-	#handles, labels = ax.get_legend_handles_labels()
-	ax.legend((("Avg. Subtract SNR "+str(SNR)+"CW "+str(peak_amplitude/noise_sigma)),("Non-subtracted SNR "+str(SNR)+"CW "+str(peak_amplitude/noise_sigma)),"Avg. Subtracted Noise","Non-subtracted Noise"))
-	#l = legend((
-	#plt.text(100,450, "Mean = "+str(noise_mean)+"mV, Sigma = "+str(noise_sigma)+"mV")
-	plt.savefig(output_dir+"/avg_sub_cw_"+str(peak_amplitude)+".png")
-	#plt.show()
-	settings_filename.close()
+	if (cw_flag):
+		ax.legend((("Avg. Subtract SNR "+str(SNR)+" CW "+str(peak_amplitude/noise_sigma)),("Non-subtracted SNR "+str(SNR)+" CW "+str(peak_amplitude/noise_sigma)),"Avg. Subtracted Noise","Non-subtracted Noise"))
+		if(save_output):
+			plt.savefig(output_dir+"/avg_sub_cw_"+str(peak_amplitude/noise_sigma)+".png")	
+	else:
+		ax.legend((("Avg. Subtract SNR "+str(SNR)),("Non-Subtracted SNR "+str(SNR)),"Avg. Subtracted Noise","Non-Subtracted Noise"))
+		if(save_output):
+			plt.savefig(output_dir+"/avg_sub_vs_non_sub.png")
+	plt.show()
+	
+	# Close open files
 	if(save_output):
+		settings_filename.close()
 		data_filename.close()
            