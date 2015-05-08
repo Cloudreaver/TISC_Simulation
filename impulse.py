fd194a0e509f1220ea45405ae7ced5d07ce44771
Ryan Hupe <ryanhupe@gmail.com>
2015-05-08 14:29:49 -0400

    Fixed random phase and shift for impulse

--- impulse.py
+++ impulse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from scipy.signal import butter, filtfilt, lfilter
+from scipy.signal import butter, filtfilt, lfilter, resample
 
 
 def butter_bandpass(lowcut, highcut, fs=2800000000.0, order=2):
@@ -43,7 +43,7 @@ def sinc_interp(x, s, u):
 
 
 
-def impulse_gen(num_samples,delay,upsample=10,freq_response=2600000000.0,low_corner_freq=132000000.0,high_corner_freq=1300000000.0,order=4,draw_flag=0,output_dir='output/'):
+def impulse_gen(num_samples,delay,upsample=10,freq_response=2600000000.0,low_corner_freq=132000000.0,high_corner_freq=900000000.0,order=2,draw_flag=0,output_dir='output/'):
     # Input defaults are set for TISC bandpass filters
 
     import numpy as np
@@ -51,6 +51,7 @@ def impulse_gen(num_samples,delay,upsample=10,freq_response=2600000000.0,low_cor
         import matplotlib.pyplot as plt
     from scipy.signal import freqz
     from scipy import ifft, shape
+    from scipy.interpolate import interp1d
 
     # Get number of samples in upscaled signal
     num_upsamples = num_samples*upsample
@@ -63,55 +64,57 @@ def impulse_gen(num_samples,delay,upsample=10,freq_response=2600000000.0,low_cor
     upsample_signal_temp = np.zeros(num_upsamples)
     upsample_signal = np.zeros(num_upsamples)
     
-    # Plot the frequency response of the filter
+    # Get time axis
+    time = np.linspace(0.0,(num_samples/freq_response)*(10**9), num_samples)
+    upsample_time = np.linspace(0.0,(num_samples/freq_response)*(10**9), num_upsamples)
+
+    b, a = butter_bandpass(low_corner_freq, high_corner_freq, order=4,fs=2600000000.0)
+    w, h = freqz(b, a, worN=num_samples)
+    
+    # Plot the frequency response of the antenna
     if(draw_flag):
         plt.figure(1)
         plt.clf()
-
-    b, a = butter_bandpass(low_corner_freq, high_corner_freq, order=order)
-    w, h = freqz(b, a, worN=num_samples)
-
-    if(draw_flag):
         plt.plot(((freq_response * 0.5 / np.pi) * w)/(10**9), abs(h), label="order = %d" % order)
         plt.axis([0,1.5,0,1.1])
         plt.xlabel('Frequency (MHz)')
         plt.ylabel('Gain')
-        plt.title('Butterworth Bandpass Filter')
+        plt.title('Antenna Impulse Reponse')
         plt.grid(True)
         
-    # Get time axis
-    time = np.linspace(0.0,(num_samples/freq_response)*(10**9), num_samples)
-    upsample_time = np.linspace(0.0,(num_samples/freq_response)*(10**9), num_upsamples)
-    
     # Transform bandpass filter into time domain to get impulse signal
-    signal_temp = ifft(h,num_samples)
+    upsample_signal_temp = ifft(h,num_upsamples).real
+        
+    # Delay impulse 
+    random_delay = int(np.random.uniform()*16)
+
+    upsample_signal[random_delay*upsample:num_upsamples] = upsample_signal_temp[0:num_upsamples-random_delay*upsample]
     
-    upsample_signal_temp = sinc_interp(signal_temp,time,upsample_time)
+    if (draw_flag==1):
+        plt.figure(2)
+        plt.plot(upsample_time,1000.0*upsample_signal[0:num_upsamples])
+        plt.xlabel("Time [ns]")
+        plt.ylabel("Voltage [mV]")
+        plt.title("Upsampled Impulse Simulation")
+        #plt.show()
+
     
     sample_period = upsample_time[1]-upsample_time[0]
     phase = int(np.random.uniform()*upsample)
-    #print phase
     
     for t in range(0,num_samples):
-        signal_temp[t] = upsample_signal_temp[t*upsample+phase]#/(0.1*(t+1)**2)
+        signal[t] = upsample_signal[t*upsample+phase]
 
     
-    
-
-    # Delay impulse 
-    random_delay = int(np.random.uniform()*16)
-    #print random_delay
-    signal[random_delay:num_samples] = signal_temp[0:num_samples-random_delay]
-    upsample_signal[random_delay*upsample:num_upsamples] = upsample_signal_temp[0:num_upsamples-random_delay*upsample]
-
     if (draw_flag==1):
-        plt.figure(2)
-        plt.plot(time,1000*signal[0:num_upsamples])
+        plt.figure(3)
+        plt.plot(time,1000.0*signal[0:num_samples])
         plt.xlabel("Time [ns]")
         plt.ylabel("Voltage [mV]")
         plt.title("Impulse Simulation")
         plt.show()
-    return signal#, upsample_signal
+    
+    return signal, upsample_signal
 
 
 if __name__ == '__main__':
@@ -122,17 +125,17 @@ if __name__ == '__main__':
     from digitizer import digitize
     from cw import generate_cw
     
-    sample_frequency = 2800000000.0
-    sample_length = 74
+    sample_frequency = 2600000000.0
+    sample_length = 32
     impulse_position = 10
-    upsample = 100
+    upsample = 10
     draw_flag = 0
     output_dir = 'output/'
     clock_jitter = 0.000000001
     #poly_index = 5
     SNR = 3
     noise_sigma = 20
-    save_plot_flag = True
+    save_plot_flag = False
     cw_flag = False
     carrier_frequency=260000000.0    # Hz
     modulation_frequency=16000000.0  # Hz
@@ -153,6 +156,7 @@ if __name__ == '__main__':
     
     time = np.linspace(0.0,((sample_length*(10**9))/sample_frequency), sample_length)
     impulse_sample, impulse_upsample = impulse_gen(sample_length,impulse_position,upsample,sample_frequency,draw_flag=draw_flag,output_dir=output_dir)
+    
     plt.figure(1)
     plt.clf()
     plt.plot(time,impulse_sample[0:sample_length])
@@ -160,8 +164,8 @@ if __name__ == '__main__':
     plt.ylabel("Amplitude [unitless]")
     plt.title("Impulse Simulation")
     if (save_plot_flag):
-        plt.savefig("impulse_sample.png")
-     
+        plt.savefig("plots/impulse_sample.png")
+    
     upsample_time = np.linspace(0.0,((upsample_length*(10**9))/(sample_frequency*upsample)), upsample_length)
     #impulse_upsample = impulse_gen(sample_length,impulse_position,upsample,sample_frequency,draw_flag=draw_flag,output_dir=output_dir)
     plt.figure(2)
@@ -171,7 +175,7 @@ if __name__ == '__main__':
     plt.ylabel("Voltage [mV]")
     plt.title("Upsampled Impulse Simulation")
     if (save_plot_flag):
-        plt.savefig("upsample_impulse_sample.png")
+        plt.savefig("plots/upsample_impulse_sample.png")
       
     signal_amp = (2*SNR*noise_sigma)
     difference=np.amax(impulse_sample)-np.amin(impulse_sample) # Get peak to peak voltage
@@ -186,7 +190,7 @@ if __name__ == '__main__':
     plt.ylabel("Voltage [mV]")
     plt.title("Band Limited Noise Sample")
     if (save_plot_flag):
-        plt.savefig("noise_sample.png")
+        plt.savefig("plots/noise_sample.png")
     
     if (cw_flag):
         cw_sample = generate_cw(sample_length=sample_length,upsample=1,sample_frequency=sample_frequency,carrier_frequency=carrier_frequency,modulation_frequency=modulation_frequency,peak_amplitude=peak_amplitude,filter_flag=1)
@@ -197,7 +201,7 @@ if __name__ == '__main__':
         plt.ylabel("Voltage [mV]")
         plt.title("Carrier Wave Sample (Peak Amp: "+str(peak_amplitude/noise_sigma)+" x Noise RMS)")
         if (save_plot_flag):
-            plt.savefig("cw_sample.png")
+            plt.savefig("plots/cw_sample.png")
         
         noise_sample = np.add(noise_sample,cw_sample)
         plt.figure(5)
@@ -207,7 +211,7 @@ if __name__ == '__main__':
         plt.ylabel("Voltage [mV]")
         plt.title("Noise+CW Sample")
         if (save_plot_flag):
-            plt.savefig("noise_cw_sample.png")
+            plt.savefig("plots/noise_cw_sample.png")
         
     impulse_noise_sample = np.add(impulse_sample,noise_sample)
     plt.figure(6)
@@ -217,7 +221,7 @@ if __name__ == '__main__':
     plt.ylabel("Voltage [mV]")
     plt.title("Impulse(SNR "+str(SNR)+") + Noise Sample(RMS "+str(noise_sigma)+"mV)")
     if (save_plot_flag):
-        plt.savefig("impulse_noise_sample.png")
+        plt.savefig("plots/impulse_noise_sample.png")
     
     
     digitized_sample = digitize(impulse_noise_sample,sample_length,upsample=1,num_bits=3,noise_mean=0.0,noise_rms=20,digitization_factor=1)
@@ -228,7 +232,7 @@ if __name__ == '__main__':
     plt.ylabel("Digital Levels [unitless]")
     plt.title("Digitized Impulse + Noise Sample")
     if (save_plot_flag):
-        plt.savefig("digitized_impulse_noise_sample.png")
+        plt.savefig("plots/digitized_impulse_noise_sample.png")
     
     #for index in range(0,sample_length):
         #if (upsample > 1):
