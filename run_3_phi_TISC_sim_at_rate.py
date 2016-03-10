02cb070812ea121a87ed6b3e0e58ce74ea3a6a55
Ryan Hupe <ryanhupe@gmail.com>
2016-03-10 16:45:28 -0500

    extra comments

--- run_3_phi_TISC_sim_at_rate.py
+++ run_3_phi_TISC_sim_at_rate.py
@@ -8,69 +8,33 @@ if __name__ == '__main__':
    import subprocess as Popen
    from array import array
    from simulation_parameters import sim_param
+   
+   # This is so that it will run on the cluster
    if(sim_param.debug==False):    
       import matplotlib
       matplotlib.use('Agg')
    
    import matplotlib.pyplot as plt
    
-   #############################################################
-   # Parameters
-   if(False):
-      num_events = 10000   # Number of events to generate per loop
-      boresight = 0
-      baseline = 0
-      angle_range = 100
-      upsample = 10                     # Upsamping factor
-      digitization_factor = 32.0       # Digitization factor for threshold(mV)
-      num_bits = 3                     # Number of bits available to digitizer
-      simulation_rate = 162500000.0          # Simulation Rate
-      event_rate = 81250000.0                 # Rate to generate events
-      
-      num_samples = 80                 # Length of Signal Window
-      noise_sigma = 32.0               # Noise Sigma in mV
-      draw_flag = False                # 1=draw event graphs, 0=don't                        
-      SNR_draw_flag = True             # 1=draw SNR graphs, 0=don't
-      delay_type_flag = 1              # 1=use GLITC delays, 0=all possible delays
-      cw_flag = False
-      carrier_frequency = 260000000.0    # Hz
-      modulation_frequency = 1000.0      # Hz
-      cw_amplitude = 2.0*noise_sigma #Peak amplitude in mV
-      average_subtract_flag = 0
-      num_trials = 2
-      threshold_to_energy = 1.0#((((digitization_factor*(10**(-3)))**2)/50.0)*(1.0/2600000000.0)*32.0)*10**12 # to get to pJ
-      
-      # These delays should be negative, since A is the top antenna
-      # and we expect an upgoing signal
-      b_input_delay = -15                # Ch B signal offset
-      c_input_delay = -17           # Ch C signal offset
-
-      low_SNR = 0.0                   # Lowest SNR
-      high_SNR = 5.0                 # Highest SNR
-      step_SNR = 1.0                # SNR interval
-
-      low_threshold = 0             # Lowest Threshold
-      high_threshold = 4095        # Highest Threshold
-      step_threshold = 1            # Threshold Interval
-      
-      window_length = 8
-      window_weight = 0.5
-   #############################################################
+  
    
-   # Set some variables
+   ######### Set some variables ###########
    num_runs = int(sim_param.num_events*(sim_param.simulation_rate/sim_param.event_rate)) 
-   num_upsamples = sim_param.upsample*sim_param.num_samples # Number of upsampled samples
+   # Number of upsampled samples
+   num_upsamples = sim_param.upsample*sim_param.num_samples 
    line_color = 1
    
+   # Generate array of thresholds
    threshold = np.linspace(sim_param.low_threshold,sim_param.high_threshold,
                            (sim_param.high_threshold-sim_param.low_threshold)
-                           /sim_param.step_threshold+1) # Generate array of thresholds
+                           /sim_param.step_threshold+1) 
    num_passed_events = np.zeros(len(threshold))
    trigger_rate = np.zeros(len(threshold))
    SNR = np.linspace(sim_param.low_SNR,sim_param.high_SNR,((sim_param.high_SNR-sim_param.low_SNR)/sim_param.step_SNR)+1)
    num_delays = [63,46]
+   #########################################
    
-   # Phi sectors have alternating baselines
+   ###### Phi sectors have alternating baselines #########
    if(sim_param.boresight==0):
       if(sim_param.baseline==0):
          abc_baseline = 0
@@ -89,8 +53,9 @@ if __name__ == '__main__':
          abc_baseline = 1
          def_baseline = 0
          ghi_baseline = 0
+   #######################################################      
          
-   # Write settings file
+   ####### Write settings file #########
    if(sim_param.cw_flag):
       output_dir = str(os.path.dirname(os.path.realpath(__file__))+"/output/on_boresight_TISC_SIM_CW"+str(cw_amplitude)+"_Num"+str(num_runs)+"_"+time.strftime('%Y_%m_%d_%H.%M.%S'))
    else:
@@ -144,14 +109,13 @@ if __name__ == '__main__':
    SNR_counter = 0
    threshold_counter = 0
 
-	####################### Step over time #############################
-
    graph_filename = open(output_dir+"/graph_output.dat","w")
    graph_filename.write("SNR,threshold,trigger_rate\n")
    data_filename = open(output_dir+"/data_output.dat","w")
    data_filename.write("time_step,SNR,thermal_noise_sigma,cw_amplitude,carrier_frequency,modulation_frequency,abc_max,abc_as_max,abc_correlation_mean,def_max,def_as_max,def_correlation_mean,ghi_max,ghi_as_max,ghi_correlation_mean")
-
-   # Start Loop over SNR values
+   ###########################################3
+   
+   ########## Start Loop over SNR values ###################
    for SNR_counter in range(0,len(SNR)):
       first_hist = []
       second_hist = []
@@ -162,7 +126,7 @@ if __name__ == '__main__':
       time_since_last_trigger = 2.0*(1.0/sim_param.event_rate)
       num_passed_events = np.zeros(len(threshold))
       
-      # Start loop over events
+      ################# Start loop over events ####################
       for timestep in range(0,num_runs):
          event_passed_flag = 0 # Reset value
          max_sum = 0
@@ -175,6 +139,7 @@ if __name__ == '__main__':
             abc_max_sum,def_max_sum,ghi_max_sum,abc_angle,def_angle,ghi_angle = TISC_sim(sim_param,SNR=0.0,seed=timestep)
             data_filename.write('\n'+str(timestep)+','+str(SNR[SNR_counter])+','+str(sim_param.noise_sigma)+','+str(sim_param.cw_amplitude)+','+str(sim_param.cw_frequency)+','+str(sim_param.modulation_frequency)+','+str(abc_max_sum)+','+str(def_max_sum)+','+str(ghi_max_sum))
             
+         # Add three phi sectors together
          three_phi_max_sum = abc_max_sum+def_max_sum+ghi_max_sum
          sum_hist.append(three_phi_max_sum)
             
@@ -184,21 +149,26 @@ if __name__ == '__main__':
                num_passed_events[threshold_counter] += 1
             elif(threshold_drop_flag == False):
                threshold_drop_flag = True 
-
+      ##### Close timestep loop ###############################33
+      """
       plt.figure(4)
       plt.hist(np.array(first_hist),bins=25)
       plt.title("SNR: %.2f"%SNR[SNR_counter])
       plt.show()
-      
+      """
       # Calculate trigger rate for current threshold
       for threshold_counter in range(0,len(threshold)):
          trigger_rate[threshold_counter] = (float(num_passed_events[threshold_counter])*float(sim_param.simulation_rate))/float(num_runs)
          graph_filename.write(str(SNR[SNR_counter])+','+str(threshold[threshold_counter])+','+str(trigger_rate[threshold_counter])+'\n')
-     
-
-      # Close Threshold Loop 
+      
+      
+      # Add individual SNR plots
       plt.figure(1)
       plt.semilogy(threshold,trigger_rate,label='SNR: '+str(SNR[SNR_counter]))
+      
+   ######## Close SNR Loop #################################3
+      
+   ####### SNR Curve details ###############################
    plt.legend()
    plt.grid(True)
    plt.xlabel("Threshold [DAC]")
@@ -217,7 +187,7 @@ if __name__ == '__main__':
          elif(sim_param.baseline==1):
             plt.title("Sim Off Boresight, Short Baseline TISC SNR Curves, Sim Rate: %1.2fMHz, Event Rate: %1.2fMHz" % (sim_param.simulation_rate/10**6,sim_param.event_rate/10**6))
    plt.savefig(str(output_dir)+"/TISC_SNR_canvas.png")
-   # Close SNR Loop
+   #########################################################
 
    # Save files and close
    settings_filename.write("\nEnd time: "+str(time.strftime('%Y_%m_%d_%H.%m.%S')))
